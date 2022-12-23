c508d728f1eafc9c05714cd3bee05e7a666c1548
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-12-23 22:22:27 +0100

    Fix `mapswap!` tests from refactor

--- test/swap_test.jl
+++ test/swap_test.jl
@@ -1,52 +1,50 @@
-using Muscle.Kernel: swap!, naiveswap!
+using Muscle: mapswap!, Naive, Vectorized
 
-@testset "Kernel" verbose = true begin
-    @testset "swap methods" verbose = true begin
-        @testset "naiveswap!" begin
-            @test begin
-                A, B = Int[], Int[]
-                naiveswap!(A, B)
-                A == [] && B == []
-            end
+@testset "Kernels" verbose = true begin
+    @testset "mapswap!" verbose = true begin
+        for impl in [Naive, Vectorized]
+            @testset "$impl implementation" begin
+                @test begin
+                    A, B = Int[], Int[]
+                    mapswap!(impl, A, B)
+                    A == [] && B == []
+                end
 
-            @test_throws BoundsError begin
-                A, B = [1, 1], [2]
-                naiveswap!(A, B)
-            end
+                @test_throws DimensionMismatch begin
+                    A, B = [1, 1], [2]
+                    mapswap!(impl, A, B)
+                end
 
-            @test begin
-                A, B = [1], [2]
-                naiveswap!(A, B)
-                A == [2] && B == [1]
-            end
+                @test begin
+                    A, B = [1], [2]
+                    mapswap!(impl, A, B)
+                    A == [2] && B == [1]
+                end
 
-            @test begin
-                A, B = fill(1, 8), fill(2, 8)
-                naiveswap!(A, B)
-                A == fill(2, 8) && B == fill(1, 8)
-            end
+                @test begin
+                    A, B = fill(1, 8), fill(2, 8)
+                    mapswap!(impl, A, B)
+                    A == fill(2, 8) && B == fill(1, 8)
+                end
 
-            @test begin
-                A, B = fill(1.0, 8), fill(2.0, 8)
-                naiveswap!(A, B)
-                A == fill(2.0, 8) && B == fill(1.0, 8)
-            end
+                @test begin
+                    A, B = fill(1.0, 8), fill(2.0, 8)
+                    mapswap!(impl, A, B)
+                    A == fill(2.0, 8) && B == fill(1.0, 8)
+                end
 
-            @test begin
-                A, B = fill(1im, 8), fill(2im, 8)
-                naiveswap!(A, B)
-                A == fill(2im, 8) && B == fill(1im, 8)
-            end
+                @test begin
+                    A, B = fill(1im, 8), fill(2im, 8)
+                    mapswap!(impl, A, B)
+                    A == fill(2im, 8) && B == fill(1im, 8)
+                end
 
-            @test begin
-                A, B = fill(1.0im, 8), fill(2.0im, 8)
-                naiveswap!(A, B)
-                A == fill(2.0im, 8) && B == fill(1.0im, 8)
+                @test begin
+                    A, B = fill(1.0im, 8), fill(2.0im, 8)
+                    mapswap!(impl, A, B)
+                    A == fill(2.0im, 8) && B == fill(1.0im, 8)
+                end
             end
         end
-
-        @testset "viswap!" begin end
     end
-
-    @testset "mapswap!" verbose = true begin end
 end
