e5cddc7d23338aaa8ffefb15b57a7bd1475973e8
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-03-07 20:18:04 +0100

    Implement Gram-Schmidt process for orthonormal basis search

---
+++ src/Numerics.jl
@@ -0,0 +1,28 @@
+using LinearAlgebra
+
+"""
+    gramschmidt!(A[, p; atol])
+
+Creates an orthogonal basis from a random matrix `A` using the numerically-stable Gram-Schmidt process.
+"""
+function gramschmidt!(A::AbstractMatrix{T}) where {T}
+    m = size(A, 1)
+    for i in 1:m-1
+        # `v` is used as reference
+        v = @view A[i:i, :] # NOTE `i:i` to keep it being a row vector
+        normalize!(v)
+
+        sA = @view A[i+1:end, :]
+
+        # `u` is the vector of projections
+        u = sA * v'
+
+        # remove `v` component with a rank-1 update
+        BLAS.ger!(-one(T), vec(u), vec(conj(v)), sA)
+    end
+
+    # normalize last row
+    eachrow(A) |> last |> normalize!
+
+    return A
+end
