4a8123900e4002348f1037de2cd847de6aa8151c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-03-19 00:10:51 +0100

    Implement `Index` types

--- src/Muscle.jl
+++ src/Muscle.jl
@@ -1,4 +1,13 @@
 module Muscle
 
+using Compat: @compat
+
+include("Index.jl")
+export NamedIndex
+export Site, issite, site, @site_str
+export Bond, isbond, bond
+export Plug, isplug, plug, isdual, @plug_str
+
+@compat public Moment
 
 end
