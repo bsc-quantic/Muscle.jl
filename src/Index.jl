c0aace4fd5f944feec40bc913554e28b14dd193a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-04-14 20:20:53 +0200

    Fixes for `Index`

--- src/Index.jl
+++ src/Index.jl
@@ -2,17 +2,20 @@ struct Index{T}
     tag::T
 end
 
+const NamedIndex = Index{Symbol}
+const TagIndex{T<:Tag} = Index{T}
+
 # TODO checkout whether this is a good idea
 Base.copy(x::Index) = x
 
 Index(name::String) = Index(Symbol(name))
 
 # index management
-function findperm(from, to)
+function findperm(from::Vector{I}, to::Vector{I}) where {I<:Index}
     @assert issetequal(from, to)
 
     # if there are hyperindices, we remove one by one
-    inds_to = collect(Union{Missing,Symbol}, to)
+    inds_to = collect(Union{Missing,I}, to)
 
     return map(from) do ind
         i = findfirst(isequal(ind), inds_to)
@@ -22,4 +25,9 @@ function findperm(from, to)
 
         i
     end
+end
+
+# required for `Tensor` constructor
+function Base.convert(::Type{ImmutableArray{Index,N}}, x::ImmutableArray{I,N}) where {I<:Index,N}
+    return ImmutableArray{Index,N}(x.data)
 end
