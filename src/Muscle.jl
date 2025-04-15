2044ba681febb6a19f721468c5551a865d65d5b2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-04-15 16:52:47 +0200

    Move `Tag` system to QuantumTags.jl

--- src/Muscle.jl
+++ src/Muscle.jl
@@ -5,15 +5,8 @@ using Compat: @compat
 
 include("Utils/Utils.jl")
 
-include("Tag.jl")
-export Site, issite, site, @site_str
-export Bond, isbond, bond, @bond_str
-export Plug, isplug, plug, isdual, @plug_str
-
 include("Index.jl")
 export Index
-
-@compat public Moment
 
 include("Tensor.jl")
 export Tensor
