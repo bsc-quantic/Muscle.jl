c6bf4c04821544ca09d9ebf965bddcaae283b880
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-04-23 17:44:02 +0200

    update `Operations.tensor_svd_thin` tests

--- test/unit/operations/tensor_svd_thin.jl
+++ test/unit/operations/tensor_svd_thin.jl
@@ -3,21 +3,21 @@ using Muscle: Tensor, Index, Operations
 
 A = Tensor(rand(ComplexF64, 2, 4, 6, 8), [Index(:i), Index(:j), Index(:k), Index(:l)])
 
-# throw if left_inds is not provided
+# throw if inds_u is not provided
 @test_throws ArgumentError Operations.tensor_svd_thin(A)
 
 # throw if index is not present
-@test_throws ArgumentError Operations.tensor_svd_thin(A; left_inds=[Index(:z)])
-@test_throws ArgumentError Operations.tensor_svd_thin(A; right_inds=[Index(:z)])
+@test_throws ArgumentError Operations.tensor_svd_thin(A; inds_u=[Index(:z)])
+@test_throws ArgumentError Operations.tensor_svd_thin(A; inds_v=[Index(:z)])
 
 # throw if no inds left
-@test_throws ArgumentError Operations.tensor_svd_thin(A; left_inds=[Index(:i), Index(:j), Index(:k), Index(:l)])
-@test_throws ArgumentError Operations.tensor_svd_thin(A; right_inds=[Index(:i), Index(:j), Index(:k), Index(:l)])
+@test_throws ArgumentError Operations.tensor_svd_thin(A; inds_u=[Index(:i), Index(:j), Index(:k), Index(:l)])
+@test_throws ArgumentError Operations.tensor_svd_thin(A; inds_v=[Index(:i), Index(:j), Index(:k), Index(:l)])
 
 # throw if chosen virtual index already present
-@test_throws ArgumentError Operations.tensor_svd_thin(A; left_inds=[Index(:i)], virtualind=Index(:j))
+@test_throws ArgumentError Operations.tensor_svd_thin(A; inds_u=[Index(:i)], ind_s=Index(:j))
 
-U, s, V = Operations.tensor_svd_thin(A; left_inds=[Index(:i), Index(:j)], virtualind=Index(:x))
+U, s, V = Operations.tensor_svd_thin(A; inds_u=[Index(:i), Index(:j)], ind_s=Index(:x))
 
 @test inds(U) == [Index(:i), Index(:j), Index(:x)]
 @test inds(s) == [Index(:x)]
