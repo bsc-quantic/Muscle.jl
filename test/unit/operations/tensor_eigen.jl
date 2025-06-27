d11a87b17d499ba134f5873baf6a679953778ae1
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-06-27 14:34:43 +0200

    update test for eigen

--- test/unit/operations/tensor_eigen.jl
+++ test/unit/operations/tensor_eigen.jl
@@ -1,55 +1,50 @@
 using Test
 using Muscle: Muscle, Tensor, Index
+using LinearAlgebra
+
+A = rand(4,4)
+L, U = eigen(A)
+A * U ≈ U * Diagonal(L)
 
 # TODO numeric test with non-random data
 # TODO test on NVIDIA GPU
 
-A = Tensor(rand(ComplexF64, 8, 4, 6, 3), [Index(:i), Index(:j), Index(:k), Index(:l)])
+Ainds = Index(:l1), Index(:l2), Index(:r1), Index(:r2)
+A = Tensor(rand(ComplexF64, 8, 4, 8, 4), Ainds)
 
 # throw if inds_u is not provided
 @test_throws ArgumentError Muscle.tensor_eigen_thin(A)
 
 # throw if index is not present
 @test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_u=[Index(:z)])
-@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_v=[Index(:z)])
+@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_uinv=[Index(:z)])
 
 # throw if no inds left
-@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_u=[Index(:i), Index(:j), Index(:k), Index(:l)])
-@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_v=[Index(:i), Index(:j), Index(:k), Index(:l)])
-
-# throw if chosen virtual index already present
-@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_u=[Index(:i)], ind_lambda=Index(:j))
+@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_u=[Index(:l1), Index(:l2), Index(:r1), Index(:r2)])
+@test_throws ArgumentError Muscle.tensor_eigen_thin(A; inds_uinv=[Index(:l1), Index(:l2), Index(:r1), Index(:r2)])
 
 # throw if non-square 
-@test_throws DimensionMismatch Muscle.tensor_eigen_thin(A; inds_u=[Index(:i), Index(:j)], ind_lambda=Index(:x))
+@test_throws DimensionMismatch Muscle.tensor_eigen_thin(A; inds_u=[Index(:l1), Index(:r1)], ind_lambda=Index(:x))
 
-lambdas, U = Muscle.tensor_eigen_thin(A; inds_u=[Index(:i), Index(:l)], ind_lambda=Index(:x))
+#Now the actual thing
+lambdas, U = Muscle.tensor_eigen_thin(A; inds_u=[Index(:l1), Index(:l2)], ind_lambda=Index(:lambda))
 
-@test inds(U) == [Index(:i), Index(:l), Index(:x)]
-@test inds(lambdas) == [Index(:x)]
+@test Muscle.inds(U) == [Index(:l1), Index(:l2), Index(:lambda)]
+@test Muscle.inds(lambdas) == [Index(:lambda)]
 
-@test size(U) == (24, 8, 3)
-@test size(lambdas) == (24,)
+@test size(U) == (8, 4, 32)
+@test size(lambdas) == (32,)
 
-#= TODO no backend and also this is sketchy
-function inv(
-             A::Tensor; inds_left=(), inds_right=() #, inplace=false, kwargs...
-       )
-         inds_left, inds_right = factorinds(inds(A), inds_left, inds_right)
-         @argcheck isdisjoint(inds_left, inds_right)
-         @argcheck issetequal(inds_left ∪ inds_right, inds(A))
+Ut = replace(U, Index(:l1) => Index(:r1), Index(:l2) => Index(:r2))
 
-         # permute array
-         left_sizes = map(Base.Fix1(size, A), inds_left)
-         right_sizes = map(Base.Fix1(size, A), inds_right)
-         Amat = permutedims(A, [inds_left..., inds_right...])
-         Amat = reshape(parent(Amat), prod(left_sizes), prod(right_sizes))
+AU = Muscle.binary_einsum(A, Ut)
+UL = Muscle.binary_einsum(U, lambdas; dims=Index[])
+# Test AU ≈ UΛ  (right eigenvectors)
+@test isapprox(AU, UL)
 
-         Ainv = LinearAlgebra.inv(Amat)
+using ITensors
+ITA = convert(ITensor, A)
+ivals, ivecs = eigen(ITA, [ind(ITA,1), ind(ITA,2)],[ind(ITA,3), ind(ITA,4)] )
+@test vector(diag(ivals)) ≈ parent(lambdas)
+@test parent(U) ≈ ITensors.tensor(ivecs)
 
-         # tensorify results
-         Ainv = Tensor(reshape(Ainv, right_sizes..., left_sizes...), [inds_right; inds_left])
-
-       end
-  =#
-  @test isapprox(Muscle.binary_einsum(A, U), Muscle.binary_einsum(lambdas, U; dims=Index[]))
