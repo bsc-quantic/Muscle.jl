43dc27d6e1baa36edf316a30583bbf6b732ba55c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-08-02 00:03:04 +0200

    try loading packages first

--- test/python/runtests.jl
+++ test/python/runtests.jl
@@ -4,6 +4,8 @@ pkg"precompile"
 
 using Test
 using SafeTestsets
+using Muscle
+using PythonCall
 
 @testset "Python integration" verbose = true begin
     @safetestset "qiskit" include("qiskit.jl")
